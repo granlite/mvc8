d0b114dbc67367f551d99fe58cd7df0ebd41ddf5
granlite <65130690+granlite@users.noreply.github.com>
2020-09-15 23:33:47 +0400

    Add files via upload

---
+++ mvc8/Controllers/StoreManagerController.cs
@@ -0,0 +1,84 @@
+﻿using muscshop.Context;
+using muscshop.filters;
+using muscshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace muscshop.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class StoreManagerController : Controller
+    {
+        StoreContext _storeContext = new StoreContext();
+        public ActionResult Index()
+        {
+            var albums = _storeContext.Albums;
+            return View(albums);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var albums = _storeContext.Albums.Where(x => x.AlbumId == id).FirstOrDefault();
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ArtistId = new SelectList(_storeContext.Artists, "ArtistId", "Name", albums.ArtistId);
+            ViewBag.GenreId = new SelectList(_storeContext.Genres, "GenreId", "Name", albums.GenreId);
+            return View(albums);
+        }
+        [HttpPost]
+        public ActionResult Edit(Album updateAlbum)
+        {
+            if (ModelState.IsValid)
+            {
+                _storeContext.Entry(updateAlbum).State = EntityState.Modified;
+                _storeContext.SaveChanges();
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return Edit(updateAlbum.AlbumId);
+            }
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ArtistId = new SelectList(_storeContext.Artists, "ArtistId", "Name", 1);
+            ViewBag.GenreId = new SelectList(_storeContext.Genres, "GenreId", "Name", 1);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Album newalbum)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<KeyValuePair<string, ModelState>> errorList = ModelState.Where(x => x.Value.Errors.Count > 0).ToList();
+                ViewBag.ErrorList = errorList;
+                return Create();
+            }
+            _storeContext.Albums.Add(newalbum);
+            _storeContext.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public ActionResult DeleteAlb(int id)
+        {
+            var deletealbum = _storeContext.Albums.Find(id); ;
+            _storeContext.Albums.Remove(deletealbum);
+            _storeContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Search(string par)
+        {
+            var albums = _storeContext.Albums.Where(x => x.Title.ToLower().Contains(par.ToLower()));
+            return View("index", albums);
+        }
+    }
+}
---
+++ mvc8/Models/Album.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace muscshop.Models
+{
+    public class Album
+    {
+        Required(ErrorMessage = "enter the title")]
+        public string Title { get; set; }
+        [Range(1, 300, ErrorMessage = "enter price: 1 to 300")]
+        public double Price { get; set; }
+        [Display(Name = "Photo URL")]
+        public string AlbumUrl { get; set; }
+        [Required(ErrorMessage = "ReleaseYear is required")]
+        [Display(Name = "Release Year")]
+        [Range(1960, 2020, ErrorMessage = "enter year: 1960 to 2020")]
+        public int ReleaseYear { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+        public int AlbumId { get; set; }
+        [MaxLength(100, ErrorMessage = "Max 100 symbols")]
+        public string Description { get; set; } = "albumDescription";
+        public int ArtistId { get; set; }
+        public int GenreId { get; set; }
+        public void UpdateAlb(Album newalbum)
+        {
+            Price = newalbum.Price;
+            Title = newalbum.Title;
+            GenreId = newalbum.GenreId;
+            ArtistId = newalbum.ArtistId;
+            ReleaseYear = newalbum.ReleaseYear;
+            Description = newalbum.Description;
+            AlbumUrl = newalbum.AlbumUrl;
+        }
+    }
+}
